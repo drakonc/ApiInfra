df0b9818d90c679b993fb0058dc2a963a28f1940
Jose Castro <drakonc@gmail.com>
2019-08-26 13:44:31 -0500

    Cambiando Datos

--- db/scripts.sql
+++ db/scripts.sql
@@ -5,6 +5,15 @@ CREATE TABLE rol (
     NombreRol VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE usuario(
+	IdUsuario VARCHAR(30) NOT NULL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    correo VARCHAR(50) NOT NULL,
+    password VARCHAR(70) NOT NULL,
+	IdRol VARCHAR(20) NOT NULL,
+    FOREIGN KEY (IdRol) REFERENCES rol(IdRol)
+);
+
 CREATE TABLE permiso(
 	IdPermiso Varchar(20) NOT NULL PRIMARY KEY,
     Permiso VARCHAR(100)
@@ -16,13 +25,4 @@ CREATE TABLE permisodenegadoporol(
     denegar BOOLEAN DEFAULT true,
     FOREIGN KEY (IdRol) REFERENCES rol(IdRol),
     FOREIGN KEY (IdPermiso) REFERENCES permiso(IdPermiso)
-);
-
-CREATE TABLE usuario(
-	IdUsuario VARCHAR(30) NOT NULL PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    correo VARCHAR(50) NOT NULL,
-    password VARCHAR(70) NOT NULL,
-	IdRol VARCHAR(20) NOT NULL,
-    FOREIGN KEY (IdRol) REFERENCES rol(IdRol)
 );
--- src/app/model/permiso.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Permiso {
-  IdPermiso: string;
-  Permiso: string;
-}
